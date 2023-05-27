14185a036e62d66730a91f83e6b68bf61fba69e9
Kelvinskell <kelvinskelll@gmail.com>
2023-05-27 15:18:07 +0100

    Associate security group

--- launch_template.tf
+++ launch_template.tf
@@ -1,5 +1,5 @@
-resource "aws_launch_template" "foo" {
-  name = "foo"
+resource "aws_launch_template" "project-x-template" {
+  name = "project-x-logic-tier-template"
 
 block_device_mappings {
     device_name = "/dev/sdf"
@@ -12,18 +12,19 @@ block_device_mappings {
   instance_type = var.instance_type
 
    network_interfaces {
+    security_groups = [aws_security_group.Allow_ALB.id]
     associate_public_ip_address = false
+    delete_on_termination = true
   }
 
  vpc_security_group_ids = [aws_security_group.Allow_ALB.id]
-
+ user_data = filebase64("./user_data.sh")
   tag_specifications {
     resource_type = "instance"
 
     tags = {
-      Name = "project-x-logic-tier-template"
+      Name = "project-x-logic-tier-template",
+      Environment = "prod"
     }
   }
-
-  user_data = file("./user_data.sh")
 }
