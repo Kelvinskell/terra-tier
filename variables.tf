a8bf093faf68ea8742edf230e21893caaffe03d7
Kelvinskell <kelvinskelll@gmail.com>
2023-05-27 14:27:23 +0100

    Update variiables

--- variables.tf
+++ variables.tf
@@ -7,3 +7,13 @@ variable "public_subnets" {
     type = list
     default = ["10.0.0.0/20", "10.0.16.0/24", "10.0.32.0/24"]
 }
+
+variable "image_id" {
+    type = string
+    default = "ami-053b0d53c279acc90"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t3.xlarge"
+}
