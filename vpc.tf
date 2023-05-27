c2ab5e719d0ed22e4b27e638a2274432db27ed43
Kelvinskell <kelvinskelll@gmail.com>
2023-05-27 11:47:46 +0100

    Add variable values for subnets

--- vpc.tf
+++ vpc.tf
@@ -5,8 +5,8 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.128.0/20", "10.0.144.0/20", "10.0.160.0/20"]
-  public_subnets  = ["10.0.0.0/20", "10.0.16.0/24", "10.0.32.0/24"]
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
 
   enable_nat_gateway = true
   single_nat_gateway = true
