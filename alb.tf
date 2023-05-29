0bd327a5e219a698b0e375758088a59916df583b
Kelvinskell <kelvinskelll@gmail.com>
2023-05-29 16:35:31 +0100

    Create targt group and attachments

--- alb.tf
+++ alb.tf
@@ -13,4 +13,25 @@ resource "aws_lb" "alb" {
   }
 }
 
+# Create a new load balancer attachment
+resource "aws_autoscaling_attachment" "asg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+  elb                    = aws_lb.alb
+
+  depends_on = [ aws_autoscaling_group.asg ]
+}
+
 # Create target group
+resource "aws_lb_target_group" "tg" {
+  name     = "tf-example-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+}
+
+# create target group attachment
+resource "aws_lb_target_group_attachment" "tg" {
+  target_group_arn = aws_lb_target_group.tg
+  target_id        = aws_lb.alb.arn
+  port             = 80
+}
