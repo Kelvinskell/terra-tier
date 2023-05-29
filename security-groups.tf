fa54dc0e4efe8a6634599a7a203c660c9beb4592
Kelvinskell <kelvinskelll@gmail.com>
2023-05-29 15:07:33 +0100

    Create security group for alb

--- security-groups.tf
+++ security-groups.tf
@@ -6,9 +6,20 @@ locals {
         protocol = "tcp"
     }
     ]
+  ingress-alb = [
+    {
+      port = 80
+      description = "Allow HTTP"
+      protocol = "tcp"
+    },
+    {
+      port = 443
+      description = "Allow HTTPS"
+      protocol = "tcp"
+    }
+  ]
 }
-
-# Security group for application layer
+# Security group for application layer servers
 resource "aws_security_group" "Allow_ALB" {
   name = "project-x-logic-tier-sg"
   description        = "Allow only authorized access to logic layer"
@@ -34,8 +45,46 @@ resource "aws_security_group" "Allow_ALB" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+ lifecycle {
+    create_before_destroy = true
+  }
+
   tags = {
     Environment = "prod"
     Name = "project-x-logic-tier-sg"
   }
+}
+
+
+# Create security group for load balancer
+resource "aws_security_group" "alb-sg" {
+  name = "project-x-logic-tier-sg"
+  description        = "Allow only authorized access to logic layer"
+  vpc_id   = module.vpc.vpc_id
+
+# Create a dynamic block
+  dynamic "ingress" {
+    for_each = local.ingress-alb
+    content {
+    description      = ingress-alb.value.description
+    from_port        = ingress-alb.value.port
+    to_port          = ingress-alb.value.port
+    protocol         = ingress-alb.value.protocol
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+}
+
+  egress {
+    from_port        = 5000
+    to_port          = 5000
+    protocol         = "tcp"
+    security_groups = [aws_security_group.Allow_ALB.id]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [ aws_security_group.Allow_ALB ]
 }
