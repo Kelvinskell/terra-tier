8f6f67c89698e43e5dfe4ab3bca3bc27d83f78fe
Kelvinskell <kelvinskelll@gmail.com>
2023-05-29 15:25:24 +0100

    Use flatten function for vpcs

--- alb.tf
+++ alb.tf
@@ -3,8 +3,8 @@ resource "aws_lb" "alb" {
   name               = "project-x-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb-sg]
-  subnets            = [for subnet in module.vpc.public_subnets : subnet.id]
+  security_groups    = [aws_security_group.alb-sg.id]
+  subnets            = flatten([module.vpc.public_subnets[*]])
 
   enable_deletion_protection = true
 
