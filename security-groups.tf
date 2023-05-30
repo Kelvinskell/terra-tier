b0f7bf75e8d9cc21eeeb8c0ee03feccff6d85f62
Kelvinskell <kelvinskelll@gmail.com>
2023-05-30 03:21:59 +0100

    edit sg for bastion host

--- security-groups.tf
+++ security-groups.tf
@@ -139,7 +139,7 @@ resource "aws_security_group" "Allow_NFS" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = [0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
