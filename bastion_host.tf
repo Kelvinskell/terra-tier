3522a14ed99b44a7e031103ab7f296ee3312c0e9
Kelvinskell <kelvinskelll@gmail.com>
2023-05-30 03:52:24 +0100

    update bastion host

---
+++ bastion_host.tf
@@ -0,0 +1,17 @@
+# Create bastion host
+resource "aws_instance" "bastion" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  # key_name = "${aws_key_pair.ec2_key.key_name}"
+  vpc_security_group_ids = [ aws_security_group.bastion-sg ]
+  # user_data = data.template_file.user_data.rendered
+  tags = {
+    Name = "project-x-Bastion-Host"
+    Environment = "prod"
+  }
+}
+
+# Print Public Ip address
+output "public_ip" {
+    value = aws_instance.bastion.public_ip
+}
