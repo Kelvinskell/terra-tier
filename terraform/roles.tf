584e16328b7ed14c1d74128cdf043dad0cd327be
Kelvinskell <kelvinskelll@gmail.com>
2023-05-31 01:42:05 +0100

    add json attribute

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -25,7 +25,7 @@ data "aws_iam_policy_document" "assume_role" {
 resource "aws_iam_role" "server_role" {
     name = "PROJECTXROLE"
     path = "/"
-    assume_role_policy = data.aws_iam_policy_document.assume_role
+    assume_role_policy = data.aws_iam_policy_document.assume_role.json
 
     inline_policy {
         name = "project_x_inline_policy_ec2_read_access"
