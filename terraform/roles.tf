6741b7ab21d3d72d281920523e8716d63faabc72
Kelvinskell <kelvinskelll@gmail.com>
2023-05-31 01:39:33 +0100

    Correct role statement

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -3,9 +3,33 @@ resource "aws_iam_instance_profile" "server_profile" {
     name = "PROJECTXLOGICTIERSERVERINSTANCEPROFILE"
     role = aws_iam_role.server_role.name
 
+    tags = {
+        Environment = "prod"
+    }
+}
+
+# Define policy document
+data "aws_iam_policy_document" "assume_role" {
+    statement {
+        effect = "Allow"
+
+        principals {
+            type = "Service"
+            identifiers = ["ec2.amazonaws.com"]
+        }
+        actions = ["sts:AssumeRole"]
+    }
+}
+
+# Create Role
+resource "aws_iam_role" "server_role" {
+    name = "PROJECTXROLE"
+    path = "/"
+    assume_role_policy = data.aws_iam_policy_document.assume_role
+
     inline_policy {
         name = "project_x_inline_policy_ec2_read_access"
-
+        
         policy = jsonencode(
             {
     "Version": "2012-10-17",
@@ -37,32 +61,8 @@ resource "aws_iam_instance_profile" "server_profile" {
     ]
 }
         )
-
-    tags = {
-        Environment = "prod"
-    }
-}
-}
-
-# Define policy document
-data "aws_iam_policy_document" "assume_role" {
-    statement {
-        effect = "Allow"
-
-        principals {
-            type = "Service"
-            identifiers = ["ec2.amazonaws.com"]
         }
-        actions = ["sts:AssumeRole"]
-    }
-}
-
-# Create Role
-resource "aws_iam_role" "server_role" {
-    name = "PROJECTXROLE"
-    path = "/"
-    assume_role_policy = data.aws_iam_policy_document.assume_role
-
+        
     tags = {
         Environment = "prod"
     }
