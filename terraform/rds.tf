58361bffc338937e66fd8234c7262c0493b5c516
Kelvinskell <kelvinskelll@gmail.com>
2023-05-31 13:40:09 +0100

    Create rds and subnet group

---
+++ terraform/rds.tf
@@ -0,0 +1,33 @@
+# create a RDS Database Instance
+resource "aws_db_instance" "mysql_instance" {
+  engine               = "mysql"
+  identifier           = "database-1"
+  allocated_storage    =  20
+  max_allocated_storage = 50
+  engine_version       = "8.0.32"
+  instance_class       = "db.m5d.xlarge"
+  username             = "project_x_user"
+  manage_master_user_password = true
+  parameter_group_name = "default.mysql8.0"
+  vpc_security_group_ids = [aws_security_group.mysql_sg]
+  allow_major_version_upgrade = true
+  skip_final_snapshot  = true
+  publicly_accessible =  false
+  backup_retention_period = 14
+  db_subnet_group_name = aws_db_subnet_group.db_group.name
+
+  tags = {
+    Name = "project-x-rds-mysqldb-instance"
+    Environment = "prod"
+  }
+}
+
+# Create db subnet group
+resource "aws_db_subnet_group" "default" {
+  name       = "project-x-rds-subnet-group"
+  subnet_ids = flatten([module.vpc.private_subnets])
+
+  tags = {
+    Environment = "prod"
+  }
+}
