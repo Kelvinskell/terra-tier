cd5335559920b91e64f06c1ede63900613003916
Kelvinskell <kelvinskelll@gmail.com>
2023-06-02 08:10:38 +0100

    Add comments

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -14,6 +14,15 @@ required_providers {
   }
 }
 
+
+##########################################################################################
+YOU MUST CHANGE THE ABOVE BACKEND TO YOUR OWN BACKEND IF YOU WANT THIS TO WORK FOR YOU.
+YOU CAN EITHER CREATE A REMOTE BACKEND ON TERRAFORM CLOUD OR WITH AN S3 BUCKET + DYNAMO DB.
+ALTERNATIVELY, YOU CAN SIMPLY CHOOSE TO USE A LOCAL BACKEND.
+EITHER WAY, YOU MUST CHANGE THIS BACKEND.
+###########################################################################################
+
+
 provider "aws" {
   region = "us-east-1"
 }
